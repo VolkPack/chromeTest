1e0287576b5198e1461bc1d1968fc5ce1e66a818
Nikita Karayev <n.karayev@sisa.samsung.com>
2017-12-14 13:49:44 -0800

    Updated Test Cases

--- src/tests/chromeinvalidloginTest.java
+++ src/tests/chromeinvalidloginTest.java
@@ -1,11 +1,30 @@
 package tests;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class chromeinvalidloginTest {
 	
+	private String LOGINID = "nikitakaraye2v"; //Enter inValid Login
+	private String PASS = "Nirvana777zero-1"; //Enter inValid Pass
+	
 	@Test
 	public void testInvalid() {
-		System.out.println("ok");
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\n.karayev\\Documents\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","/resources/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+		driver.findElement(By.id("gb_70")).click();
+		driver.findElement(By.id("identifierId")).sendKeys(LOGINID);
+		driver.findElement(By.id("identifierNext")).click();
+
+		//driver.close();
 	}
 
 }
--- src/tests/chromeloginTest.java
+++ src/tests/chromeloginTest.java
@@ -1,6 +1,10 @@
 package tests;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,7 +13,7 @@ public class chromeloginTest {
 	
 	private String LOGINID = "***"; //Enter Valid Login
 	private String PASS = "***"; //Enter Valid Pass
-
+	
 	
 	@BeforeClass
 	public static void preSet() {
@@ -25,9 +29,16 @@ public class chromeloginTest {
 		driver.findElement(By.id("gb_70")).click();
 		driver.findElement(By.id("identifierId")).sendKeys(LOGINID);
 		driver.findElement(By.id("identifierNext")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(PASS);
 		driver.findElement(By.id("passwordNext")).click();
 		driver.close();
 	}
+	
+	@AfterClass
+	public static void CleanUp() {
+		
+	}
+	
 
 }
